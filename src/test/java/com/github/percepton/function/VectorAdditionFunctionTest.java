5fc9d761adf6180f143f78ae4c53c9e171f95c41
yakka34 <tv.lehtinen@hotmail.com>
2026-06-30 00:07:21 +0300

    Clean test imports

--- src/test/java/com/github/percepton/function/VectorAdditionFunctionTest.java
+++ src/test/java/com/github/percepton/function/VectorAdditionFunctionTest.java
@@ -2,7 +2,8 @@ package com.github.percepton.function;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class VectorAdditionFunctionTest {
 
